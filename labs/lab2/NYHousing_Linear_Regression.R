a5425ccc67c349d551f614d25f1ce7080700f042
Ethan Garcia <ethan2917@gmail.com>
2025-02-10 19:18:42 -0500

    Finish Lab 2

--- labs/lab2/NYHousing_Linear_Regression.R
+++ labs/lab2/NYHousing_Linear_Regression.R
@@ -1,9 +1,6 @@
 library("ggplot2")
 library("readr")
 
-# -------------------------------------------------------------------------
-
-
 ## read dataset
 NY_House_Dataset <- read_csv("NY-House-Dataset.csv")
 
@@ -22,29 +19,71 @@ dataset$PROPERTYSQFT[dataset$BROKERTITLE=="Brokered by Douglas Elliman - 575 Mad
 ## column names
 names(dataset)
 
-## fit linear model
-lmod <- lm(PRICE~PROPERTYSQFT, data = dataset)
+# Models
+## Model 1 Using PROPERTYSQFT as the predictor
+m1 <- lm(log10(PRICE)~log10(PROPERTYSQFT), data = dataset)
+## Model 2 Using PROPERTYSQFT + BEDS as the predictor
+m2 <- lm(log10(PRICE) ~ log10(PROPERTYSQFT) + BEDS, data = dataset)
+## Model 3 Using BATHS + BEDS as the predictor
+m3 <- lm(log10(PRICE) ~ BATH + BEDS, data = dataset)
 
-lmod <- lm(log10(PRICE)~log10(PROPERTYSQFT), data = dataset)
 
-## print model output
-summary(lmod)
+## Print model output
+cat("Model 1 Summary\n")
+print(summary(m1))
+cat("Model 2 Summary\n")
+print(summary(m2))
+cat("Model 3 Summary\n")
+print(summary(m3))
 
-## scatter plot of 2 variables
-plot(PRICE~PROPERTYSQFT, data = dataset)
-abline(lmod)
-
-plot(log10(PRICE)~log10(PROPERTYSQFT), data = dataset)
-abline(lmod)
-
-## scatter plot of 2 variables
-ggplot(dataset, aes(x = PROPERTYSQFT, y = PRICE)) +
-  geom_point()
-
-ggplot(dataset, aes(x = PROPERTYSQFT, y = PRICE)) +
-  geom_point() +
-  stat_smooth(method = "lm", col="red")
-
+# Plot of variables with best fit line
+# M1 using PROPERTYSQFT as most significant variable
 ggplot(dataset, aes(x = log10(PROPERTYSQFT), y = log10(PRICE))) +
   geom_point() +
-  stat_smooth(method = "lm", col="red")
+  stat_smooth(method = "lm", col = "red") +
+  labs(title = "Model 1: log10(PRICE) vs. log10(PROPERTYSQFT)",
+       x = "log10(Property SqFt)",
+       y = "log10(Price)")
+
+# M2 using BEDS as most significant variable
+ggplot(dataset, aes(x = BEDS, y = log10(PRICE))) +
+  geom_point() +
+  stat_smooth(method = "lm", col = "red") +
+  labs(title = "Model 2: log10(PRICE) vs. BEDS",
+       x = "BEDS",
+       y = "log10(Price)")
+
+# M3 using BATH as most significant variable
+ggplot(dataset, aes(x = BATH, y = log10(PRICE))) +
+  geom_point() +
+  stat_smooth(method = "lm", col = "red") +
+  labs(title = "Model 3: log10(PRICE) vs. BATH",
+       x = "Bath",
+       y = "log10(Price)")
+
+## Scatter plot of residuals
+# M1
+res1 <- data.frame(Fitted = fitted(m1), Residuals = residuals(m1))
+ggplot(res1, aes(x = Fitted, y = Residuals)) +
+  geom_point() +
+  geom_hline(yintercept = 0, linetype = "dashed", col = "blue") +
+  labs(title = "Model 1: Residuals vs. Fitted Values",
+       x = "Fitted Values",
+       y = "Residuals")
+
+res2 <- data.frame(Fitted = fitted(m2), Residuals = residuals(m2))
+ggplot(res2, aes(x = Fitted, y = Residuals)) +
+  geom_point() +
+  geom_hline(yintercept = 0, linetype = "dashed", col = "blue") +
+  labs(title = "Model 2: Residuals vs. Fitted Values",
+       x = "Fitted Values",
+       y = "Residuals")
+
+res3 <- data.frame(Fitted = fitted(m3), Residuals = residuals(m3))
+ggplot(res3, aes(x = Fitted, y = Residuals)) +
+  geom_point() +
+  geom_hline(yintercept = 0, linetype = "dashed", col = "blue") +
+  labs(title = "Model 3: Residuals vs. Fitted Values",
+       x = "Fitted Values",
+       y = "Residuals")
+
